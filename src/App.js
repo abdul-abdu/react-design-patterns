9c2cf90cc69421588265b1ffda7c8b418992cbee
Abdug'affor Abdurahimov <abdurahimov.97@list.ru>
2021-09-28 07:27:17 +0500

    Initial update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { SplitScreen } from "./SplitScreen";
+
+function LeftHandComponent() {
+  return <h1 style={{backgroundColor: "thistle"}}>LEFT</h1>;
+}
+
+function RightHandComponent() {
+  return <h1 style={{backgroundColor: "tomato"}}>Right</h1>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <SplitScreen left={LeftHandComponent} right={RightHandComponent} />;
 }
 
 export default App;
